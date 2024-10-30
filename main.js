6d577b8f3f021d5ec8af3635e642a5231a90d5c9
Jan Zípek <jan@zipek.cz>
2024-10-30 21:22:14 +0100

    Micro survivors

---
+++ main.js
@@ -0,0 +1,199 @@
+const width = 400;
+const height = 400;
+
+const canvas = document.createElement("canvas");
+canvas.width = width;
+canvas.height = height;
+
+const ctx = canvas.getContext("2d");
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      input.up = true;
+      break;
+    case "ArrowDown":
+      input.down = true;
+      break;
+    case "ArrowLeft":
+      input.left = true;
+      break;
+    case "ArrowRight":
+      input.right = true;
+      break;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      input.up = false;
+      break;
+    case "ArrowDown":
+      input.down = false;
+      break;
+    case "ArrowLeft":
+      input.left = false;
+      break;
+    case "ArrowRight":
+      input.right = false;
+      break;
+  }
+});
+
+const player = {
+  x: 0,
+  y: 0,
+  level: 0,
+  experience: 0,
+  health: 100,
+  speed: 12,
+};
+
+const input = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+const state = {
+  runtime: 0,
+  spawnTimeout: 0,
+};
+
+/** @typedef {{ health: number; speed: number; damage: number; damageTick: number; }} EnemyType */
+/** @type {EnemyType[]} */
+const enemyTypes = [
+  {
+    health: 10,
+    speed: 10,
+    damage: 1,
+    damageTick: 1,
+  },
+];
+
+/** @typedef {{ x: number; y: number; type: number; health: number; damageTick: number; }} Enemy */
+/** @type {Enemy[]} */
+const enemies = [];
+
+function renderPlayer() {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(player.x - 5, player.y - 5, 10, 10);
+}
+
+function renderEnemies() {
+  ctx.fillStyle = "#aaa";
+
+  for (const enemy of enemies) {
+    ctx.fillRect(enemy.x - 5, enemy.y - 5, 10, 10);
+  }
+}
+
+function render() {
+  ctx.reset();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(-player.x + width / 2, -player.y + height / 2);
+
+  renderPlayer();
+  renderEnemies();
+}
+
+function gameLogicTick(deltaTime) {
+  managerTick(deltaTime);
+  enemiesTick(deltaTime);
+  playerTick(deltaTime);
+}
+
+function enemiesTick(deltaTime) {
+  for (const enemy of enemies) {
+    const type = enemyTypes[enemy.type];
+
+    const dx = player.x - enemy.x;
+    const dy = player.y - enemy.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // When ready to attack
+    if (enemy.damageTick <= 0) {
+      // Damage player when close
+      if (distance < 10) {
+        player.health -= type.health;
+        enemy.damageTick = type.damageTick;
+      }
+
+      // Move towards player
+      const speed = type.speed * deltaTime;
+      enemy.x += (dx / distance) * speed;
+      enemy.y += (dy / distance) * speed;
+    } else {
+      // Reset attack tick
+      enemy.damageTick -= deltaTime;
+    }
+  }
+}
+
+function managerTick(deltaTime) {
+  state.runtime += deltaTime;
+  state.spawnTimeout += deltaTime;
+
+  if (state.spawnTimeout > 2) {
+    const angle = Math.random() * Math.PI * 2;
+    const distance = Math.max(width, height) / 2 + Math.random() * 100;
+
+    enemies.push({
+      x: player.x + Math.cos(angle) * distance,
+      y: player.y + Math.sin(angle) * distance,
+      type: 0,
+      health: 10,
+      damageTick: 0,
+    });
+
+    state.spawnTimeout = 0;
+  }
+}
+
+function playerTick(deltaTime) {
+  let moveX = 0;
+  let moveY = 0;
+
+  if (input.up) {
+    moveY -= 1;
+  }
+  if (input.down) {
+    moveY += 1;
+  }
+  if (input.left) {
+    moveX -= 1;
+  }
+  if (input.right) {
+    moveX += 1;
+  }
+
+  const speed = player.speed * deltaTime;
+  const moveD = Math.sqrt(moveX * moveX + moveY * moveY);
+
+  if (moveD > 0) {
+    player.x += (moveX / moveD) * speed;
+    player.y += (moveY / moveD) * speed;
+  }
+}
+
+let lastTime = 0;
+
+/**
+ * @param {DOMHighResTimeStamp?} time
+ */
+function animationFrameTick(nextTime) {
+  requestAnimationFrame(animationFrameTick);
+
+  const delta = (nextTime - lastTime) / 1000;
+  lastTime = nextTime;
+
+  render();
+  gameLogicTick(delta);
+}
+
+window.addEventListener("load", () => {
+  document.body.appendChild(canvas);
+  requestAnimationFrame(animationFrameTick);
+});
